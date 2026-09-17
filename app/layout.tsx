2ef3fed9f53b2d4463ba2a4c7de5b24ee7bc0fa5
sidjamyl <nj_sid@esi.dz>
2026-09-17 21:11:21 +0200

    fix: load store settings at runtime

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,8 @@ export const metadata: Metadata = {
 
 import { getSiteSettings } from "@/lib/actions";
 
+export const dynamic = 'force-dynamic';
+
 export default async function RootLayout({
   children,
 }: Readonly<{
